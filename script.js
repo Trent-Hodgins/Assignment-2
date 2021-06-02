3121ee091763253854e4b6d2c2fbf6b960420014
TrentHodgins <>
2021-06-02 21:01:11 +0000

    Fixed Errors

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById('timeline').addEventListener('click',timeline)
-document.getElementById('analytical').addEventListener('click',analytical)
-document.getElementById('difference').addEventListener('click',difference)
+document.getElementById('timeline').addEventListener('click', timeline)
+document.getElementById('analytical').addEventListener('click', analytical)
+document.getElementById('difference').addEventListener('click', difference)
 let answer
-//The let answer, allows answer to be determined. The three event listers above the "let" statment allows teh Javascript to locate the information needed to gather from the HTML document.//
+// The let answer, allows answer to be determined. The three event listers above the "let" statment allows teh Javascript to locate the information needed to gather from the HTML document.//
 
 function timeline () {
   answer = confirm('Timeline')
@@ -25,4 +25,4 @@ function difference () {
   }
 }
 
-//These three fuctions are similar, they do the same thing, the only difference is the name, button, and link. They make it that when a button is clicked you get taken to a different page on the website.//
+// These three fuctions are similar, they do the same thing, the only difference is the name, button, and link. They make it that when a button is clicked you get taken to a different page on the website.//
